ee364c39716d304b7b557c966c792dbf79c6db4c
Sabrina Lima <Sabrina Lima>
2016-03-31 19:28:56 -0300

    alterado host para localhost

--- connection.php
+++ connection.php
@@ -1,11 +1,10 @@
 <?php
-	$host = "mysql://$OPENSHIFT_MYSQL_DB_HOST:$OPENSHIFT_MYSQL_DB_PORT/";
+	$host = "localhost";
 	$user = "adminwabBZLt";
 	$pass = "ijURQcrM_epL";
 	$banco = "testedemercado";
 	
 	$conexao = mysql_connect($host, $user, $pass) or die(mysql_error());
 
-	mysql_select_db($banco) or die(mysql_error());
-	
+	mysql_select_db($banco) or die(mysql_error());	
 ?>
